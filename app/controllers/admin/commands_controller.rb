8d2cd2f5a06b9926ae9dc7d3f1b7239fcadb1dc1
Hampton Catlin <hcatlin@greed-2.local>
2009-03-07 12:40:05 -0500

    adding in yaml output

--- app/controllers/admin/commands_controller.rb
+++ app/controllers/admin/commands_controller.rb
@@ -4,6 +4,18 @@ class Admin::CommandsController < ApplicationController
   make_resourceful do
     build :all
     
+    response_for :index do |format|
+      format.html
+      format.yaml do
+        @commands.collect! do |command|
+          a = command.attributes
+          a.delete('id')
+          a
+        end
+        render :text => @commands.to_yaml
+      end
+    end
+    
     response_for :create, :update, :destroy do
       redirect_to admin_commands_path
     end
