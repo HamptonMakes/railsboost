c25343eb3f44316363cbff0a8784d4d169d992bb
Hampton Catlin <hcatlin@greed-2.local>
2009-03-11 17:04:37 -0400

    mailto links to me

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -1,10 +1,10 @@
 Mime::Type.register "text", :rb
 
-
 class TemplatesController < ApplicationController
   make_resourceful do
     build :all, :except => [:delete, :update, :edit]
     
+    # Standard response block
     response_for :show do |format|
       format.html
       format.rb { render :text => @rails_template.to_ruby }
@@ -13,6 +13,8 @@ class TemplatesController < ApplicationController
   
  private
  
+  # Override the default instance variable name, 
+  # because Rails uses @template apparently...
   def instance_variable_name
     "rails_templates"
   end
