8c64789ddada91360e54144cfeec0384299b8336
Robert Pitts <robert.pitts@bluetie.com>
2009-04-06 18:13:46 -0400

    added social junq

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,12 @@ module ApplicationHelper
   def tweet_this
     link_to(image_tag('tweet_this.png', :alt => 'Tweet This!'), "http://twitter.com/home?status=Easy Rails Template Generation at www.railsboost.com!")
   end
+  
+  def delicious_this
+    link_to(image_tag('delicious_this.png', :alt => 'Del.icio.us'), "http://del.icio.us/post?v=2&url=www.railsboost.com&title=Rails Boost: Rails Template Generator")
+  end
+  
+  def digg_this
+    link_to(image_tag('digg_this.png', :alt => 'Digg This!'), "http://digg.com/submit?phase=2&url=www.railsboost.com")
+  end
 end
