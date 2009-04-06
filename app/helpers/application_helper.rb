19e443dc62b9ba2dd7f5ba87d1ca4648b1847e24
Robert Pitts <robert.pitts@bluetie.com>
2009-04-06 16:11:51 -0400

    tweet this link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def tweet_this
+    link_to(image_tag('tweet_this.png', :alt => 'Tweet This!'), "http://twitter.com/home?status=Easy Rails Template Generation at www.railsboost.com!")
+  end
 end
